988197c39f69a9c8ed68418279c1ee74e9ebca7d
Yomlk <lastik52@mail.ru>
2024-03-15 17:58:29 +0300

    3 задача решена

--- js/arrays.js
+++ js/arrays.js
@@ -1,10 +1,1 @@
 
-//3-ая Задача
-function task3(){
-    let arr = Array.from(Array(3),()=>{return Math.round(Math.random()*20)});
-    console.log(arr.toString());
-    function proverka(arr){
-        return arr.every((item)=>item%2==0);
-    }
-    console.log(proverka(arr));
-}
